25caf5628141cf4ef591d826cfe80f8bbfd933c4
GustavoPD <gudalmoro@gmail.com>
2022-09-22 01:54:06 -0300

    ratings e menu

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,8 +1,36 @@
+from tech_news.database import find_news
+
+
 # Requisito 10
 def top_5_news():
-    """Seu código deve vir aqui"""
+    result = find_news()
+    sorted_news = sorted(
+        result, key=lambda data: data["comments_count"], reverse=True
+    )[:5]
+    news = []
+
+    for new in sorted_news:
+        news.append((new["title"], new["url"]))
+
+    return news
 
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    result = find_news()
+    categories = dict()
+    news = []
+
+    for new in result:
+        if new['category'] in categories:
+            categories[new['category']] += 1
+        else:
+            categories[new["category"]] = 0
+
+    sorted_categories = sorted(
+        categories.items(), key=lambda x: (-x[1], x[0]))
+
+    for new in sorted_categories:
+        news.append(new[0])
+
+    return news
