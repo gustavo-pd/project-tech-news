232e434d16afd8cba828cbb373d1bc41bdd24b4a
GustavoPD <gudalmoro@gmail.com>
2022-09-22 01:18:24 -0300

    scrape noticia e get tech news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,7 @@
 import requests
 import time
 from parsel import Selector
+from tech_news.database import create_news
 
 
 # Requisito 1
@@ -22,8 +23,8 @@ def fetch(url):
 
 # Requisito 2
 def scrape_novidades(html_content):
-    parsel_selector = Selector(text=html_content)
-    url = parsel_selector.css("a.cs-overlay-link::attr(href)").getall()
+    selector = Selector(text=html_content)
+    url = selector.css("a.cs-overlay-link::attr(href)").getall()
     return url
 
 
@@ -37,9 +38,40 @@ def scrape_next_page_link(html_content):
 
 # Requisito 4
 def scrape_noticia(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+
+    return {
+        "url": selector.css("link[rel=canonical] ::attr(href)").get(),
+        "title": selector.css("h1.entry-title ::text").get().strip(),
+        "timestamp": selector.css("li.meta-date ::text").get(),
+        "writer": selector.css("span.author > a.url::text").get(),
+        "comments_count": len(selector.css("div.comment-body").getall()),
+        "summary": "".join(selector.css(
+            ".entry-content > p:nth-of-type(1) ::text").getall()).strip(),
+        "tags": selector.css(
+            "section.post-tags > ul > li > a ::text").getall(),
+        "category": selector.css("a.category-style > span.label ::text").get()
+    }
 
 
 # Requisito 5
 def get_tech_news(amount):
-    """Seu código deve vir aqui"""
+    list_news = []
+    url = "https://blog.betrybe.com/"
+    response = fetch(url)
+    next_page_link = scrape_next_page_link(response)
+    news = scrape_novidades(response)
+
+    while len(news) < amount:
+        next_page = fetch(next_page_link)
+        next_news = scrape_novidades(next_page)
+        news += next_news
+        next_page_link = scrape_next_page_link(next_page)
+
+    for index in range(0, amount):
+        news_link = fetch(news[index])
+        news_scraped = scrape_noticia(news_link)
+        list_news.append(news_scraped)
+
+    create_news(list_news)
+    return list_news
